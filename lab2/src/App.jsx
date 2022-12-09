9c4a839dd9d3d1430899ffea33cc1b2db5ae2319
anegin <anegamega@gmail.com>
2022-12-09 17:12:54 +0200

    Lab2: add form default data.

--- lab2/src/App.jsx
+++ lab2/src/App.jsx
@@ -1,29 +1,91 @@
+import {useFieldArray, useForm} from 'react-hook-form';
 import './App.css'
 
 function App() {
+    const {control, register, handleSubmit} = useForm({
+        defaultValues: {
+            experts: [
+                {
+                    selfEsteem: 9,
+                    rating: 90,
+                    comment: 'Чудово відпочив',
+                },
+                {
+                    selfEsteem: 3,
+                    rating: 30,
+                    comment: 'Послали в "гарячу" точку',
+                },
+                {
+                    selfEsteem: 9,
+                    rating: 60,
+                    comment: 'Непогано за свої гроші',
+                },
+                {
+                    selfEsteem: 7,
+                    rating: 80,
+                    comment: 'Добре годували, добре поїли',
+                },
+                {
+                    selfEsteem: 5,
+                    rating: 70,
+                    comment: 'Затримали рейс, але в цілому нормально',
+                },
+                {
+                    selfEsteem: 8,
+                    rating: 70,
+                    comment: 'Харчування - не дуже',
+                },
+                {
+                    selfEsteem: 9,
+                    rating: 50,
+                    comment: 'Обіцяли 5-ти зірковий готель, а на місці виявився 3-ох',
+                },
+                {
+                    selfEsteem: 7,
+                    rating: 70,
+                    comment: 'Погане відношення персоналу, хоча відпочинок виявився непоганий',
+                },
+            ],
+        }
+    });
+
+    const {fields, append} = useFieldArray({
+        control,
+        name: 'experts',
+    });
+
     return (
         <div className="container mx-auto px-2 my-8">
             <h1 className="inline-block mb-4 text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">Застосування
                 методу "Делфі"</h1>
-            <div>Якісь вказівки</div>
-            <div>
+            <div className="my-12">Якісь вказівки</div>
+            <div className="my-12">
                 <div className="grid grid-cols-6 text-center">
                     <div>№ Експерта</div>
                     <div>Коефіцієнт самооцінки</div>
                     <div>Рівень обслуговування</div>
                     <div className="col-end-7 col-span-3">Коментар</div>
                 </div>
-                <div className="grid grid-cols-6 text-center">
-                    <div>1</div>
-                    <div><input type="number" defaultValue="10"/></div>
-                    <div><input type="number"/></div>
-                    <div className="col-end-7 col-span-3">
-                        <textarea name="comment" id="comment"></textarea>
-                    </div>
-                </div>
-                <button>Обчислити результат</button>
+                <form onSubmit={handleSubmit((data) => console.log(data))}>
+                    {fields.map((field, index) => (
+                        <div key={field.id}
+                             className="grid grid-cols-6 text-center">
+                            <div>{index + 1}</div>
+                            <div>
+                                <input {...register(`experts.${index}.selfEsteem`)}/>
+                            </div>
+                            <div>
+                                <input {...register(`experts.${index}.rating`)}/>
+                            </div>
+                            <div className="col-end-7 col-span-3">
+                                <textarea {...register(`experts.${index}.comment`)}></textarea>
+                            </div>
+                        </div>
+                    ))}
+                    <button type="submit">Обчислити результат</button>
+                </form>
             </div>
-            <div>
+            <div className="my-12">
                 <p>Середньогрупова оцінка: </p>
                 <p>Середнє значення оцінки послуг: </p>
                 <p>Середньозважена оцінка попиту: </p>
